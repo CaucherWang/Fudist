d45042e0a4486ee7de0edf899c4a837bf2a566ae
hrxiong <hrxiong73@gmail.com>
2023-06-04 14:49:50 +0000

    add finger.

--- script/index_hnsw.sh
+++ script/index_hnsw.sh
@@ -2,8 +2,8 @@
 cd ..
 
 efConstruction=500
-M=48
-data='nuswide'
+M=16
+data='gist'
 
 g++ -o ./src/index_hnsw ./src/index_hnsw.cpp -I ./src/ -O3 -mavx2 -fopenmp
 
@@ -17,14 +17,14 @@ index_path=./data/${data}
 # index_file="${index_path}/${data}_ef${efConstruction}_M${M}.index"
 # ./src/index_hnsw -d $data_file -i $index_file -e $efConstruction -m $M
 
-data_file="${data_path}/O${data}_base.fvecs"
-index_file="${index_path}/O${data}_ef${efConstruction}_M${M}.index"
+data_file="${data_path}/${data}_base.fvecs"
+index_file="${index_path}/${data}_ef${efConstruction}_M${M}.index"
 ./src/index_hnsw -d $data_file -i $index_file -e $efConstruction -m $M
 
 # data_file="${data_path}/PCA_${data}_base.fvecs"
 # index_file="${index_path}/PCA_${data}_ef${efConstruction}_M${M}.index"
 # ./src/index_hnsw -d $data_file -i $index_file -e $efConstruction -m $M
 
-data_file="${data_path}/DWT_${data}_base.fvecs"
-index_file="${index_path}/DWT_${data}_ef${efConstruction}_M${M}.index"
-./src/index_hnsw -d $data_file -i $index_file -e $efConstruction -m $M
+# data_file="${data_path}/DWT_${data}_base.fvecs"
+# index_file="${index_path}/DWT_${data}_ef${efConstruction}_M${M}.index"
+# ./src/index_hnsw -d $data_file -i $index_file -e $efConstruction -m $M
